11c5f4455f914b291a6187a1ffa115cc56c69c1d
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-09 11:54:52 +0900

    techpack: display: msm: Adapt exposure adjustment methods for sm8250
    - This commit was originally made for davinci but it helps for us too as it retains backlight level prior to enabling DC Dim.
    
    [muralivijay: Adapt to spes]
    [fadlyas07: Remove config guards.]
    [johnmart19: Adapt for sm8250]
    
    Co-authored-by: muralivijay <muralidharan9845@gmail.com>
    Co-authored-by: fadlyas07 <mhmmdfdlyas@gmail.com>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    (cherry picked from commit 0c9cb517a44473b4c65ba8f1bf9cab828e2fb182)

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -19,6 +19,14 @@
 #include "dsi_display.h"
 #include "xiaomi_frame_stat.h"
 
+#if defined(CONFIG_DRM_DYNAMIC_REFRESH_RATE)
+static struct blocking_notifier_head dsi_freq_head =
+			BLOCKING_NOTIFIER_INIT(dsi_freq_head);
+EXPORT_SYMBOL_GPL(dsi_freq_head);
+#endif
+
+#include "exposure_adjustment.h"
+
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -945,6 +953,7 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 {
 	int rc = 0;
+	int bl_dc_min = panel->bl_config.bl_min_level * 2;
 	struct dsi_backlight_config *bl = &panel->bl_config;
 	struct dsi_backlight_config *bl_slaver = &panel->bl_slaver_config;
 	struct dsi_panel_mi_cfg *mi_cfg = &panel->mi_cfg;
@@ -952,7 +961,10 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 	if (panel->host_config.ext_bridge_mode)
 		return 0;
 
-	DSI_INFO("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
+	if (bl_lvl > 0)
+                bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
+                
+	DSI_DEBUG("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
 
 	/* lmi panel must restore to last_bl_level to avoid flash high
 	 * brightness white exiting app lock with DC on (MIUI-1755728),

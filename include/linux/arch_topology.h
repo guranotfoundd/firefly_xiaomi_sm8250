028adcfd88557c5b810330b895b70fd799d84801
Lukasz Luba <lukasz.luba@arm.com>
2021-11-09 19:57:10 +0000

    arch_topology: Introduce thermal pressure update function
    The thermal pressure is a mechanism which is used for providing
    information about reduced CPU performance to the scheduler. Usually code
    has to convert the value from frequency units into capacity units,
    which are understandable by the scheduler. Create a common conversion code
    which can be just used via a handy API.
    
    Internally, the topology_update_thermal_pressure() operates on frequency
    in MHz and max CPU frequency is taken from 'freq_factor' (per-cpu).
    
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Thara Gopinath <thara.gopinath@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/arch_topology.h
+++ include/linux/arch_topology.h
@@ -25,6 +25,19 @@ unsigned long topology_get_cpu_scale(struct sched_domain *sd, int cpu)
 
 void topology_set_cpu_scale(unsigned int cpu, unsigned long capacity);
 
+DECLARE_PER_CPU(unsigned long, thermal_pressure);
+
+static inline
+unsigned long topology_get_thermal_pressure(int cpu)
+{
+	return per_cpu(thermal_pressure, cpu);
+}
+
+void topology_set_thermal_pressure(const struct cpumask *cpus, unsigned long th_pressure);
+
+void topology_update_thermal_pressure(const struct cpumask *cpus,
+				      unsigned long capped_freq);
+					  
 DECLARE_PER_CPU(unsigned long, freq_scale);
 
 static inline

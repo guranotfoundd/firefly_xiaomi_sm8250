5155a80432269d73b764fd838107ba34099a24eb
IzumiYouka <nymphbeeru@gmail.com>
2026-07-07 15:53:53 +0700

    fixup: cass: remove unused uc_min variable; gitignore: add .env

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -136,7 +136,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	struct cass_cpu_cand cands[2], *best = cands;
 	int this_cpu = raw_smp_processor_id();
 	bool has_idle = false;
-	unsigned long p_util, uc_min;
+	unsigned long p_util;
 	int cidx = 0, cpu, prev_llc_id;
 
 	/*

8ffc8f63a9dc7eade2092c57c682df3b406150c4
IzumiYouka <nymphbeeru@gmail.com>
2026-07-28 17:24:49 +0700

    Revert "block: zram: hardcode zram block size to 4gb"
    This reverts commit b1f4121392a8001dc9a855363c4c7c1dd24c28ba.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1866,8 +1866,9 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = PAGE_ALIGN((u64)6 * SZ_1G);
-	pr_info("Setting zRAM size to %li GB", disksize / 1073741824);
+	disksize = memparse(buf, NULL);
+	if (!disksize)
+		return -EINVAL;
 
 	down_write(&zram->init_lock);
 	if (init_done(zram)) {

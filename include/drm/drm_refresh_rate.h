b6248923e3ac58143e0d7f15e525daaa41602b30
IzumiYouka <nymphbeeru@gmail.com>
2026-08-14 02:56:27 +0700

    [SQUASH] Revert FAS (Frame Aware Scaling)to prepare newest implementation
    This reverts commit b8f44b826a50bbbd4707e33e8822c647b26e239c.

--- include/drm/drm_refresh_rate.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __DRM_REFRESH_RATE_H__
-#define __DRM_REFRESH_RATE_H__
-
-unsigned int dsi_panel_get_refresh_rate(void);
-
-#endif /* __DRM_REFRESH_RATE_H__ */
